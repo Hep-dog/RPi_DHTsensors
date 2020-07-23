6b0cffa7c1eab9de025ac4c374e04dc713b37445
Shen Peixun <shenpx@ihep.ac.cn>
2020-07-23 21:25:57 +0800

    Update the scripts for synchronizing

--- Sync/trans.sh
+++ Sync/trans.sh
@@ -2,8 +2,8 @@
 
 remote_ip=192.168.43.84
 
-/usr/bin/inotifywait -mrq -e modify,delete,create,attrib,move /home/pi/Work/RPi_DHTsensors/Collection/data/ | while read events
+/usr/bin/inotifywait -mrq -e modify,delete,create,attrib,move /home/jiyizi/Work/Coding/Raspberry/RPi_DHTsensors/Collection/data/ | while read events
 do
-	sudo rsync -av --password-file=/etc/rsyncd.secrets /home/pi/Work/RPi_DHTsensors/Collection/data/ pi@${remote_ip}::dht11
+	sudo rsync -av --password-file=/etc/rsyncd.secrets /home/jiyizi/Work/Coding/Raspberry/RPi_DHTsensors/Collection/data/ pi@${remote_ip}::dht11
 	sudo echo "`date +'%F %T'` have $events" >> /tmp/rsync.log 2>&1
 done
