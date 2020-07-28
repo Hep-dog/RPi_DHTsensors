2a9ae1873cf6630638c967584066dedec02f7c5e
Jiyizi <shenpx91@gmail.com>
2020-07-28 23:47:30 +0800

    Update the script clearninig log files of RPi

--- Setup/Clean_log.py
+++ Setup/Clean_log.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python
 
+'''
+    This script is used to keep the system file size of Raspberry Pi
+    The target file is 'syslog' and 'daemon.log' in /var/log/ dir
+'''
+
 import os 
 
 CMDs = []
 
 CMDs.append('sudo tail -n 1000 /var/log/syslog > /tmp/temp')
 CMDs.append('sudo mv /tmp/temp /var/log/syslog')
-CMDs.append('sudo chmod 600 /var/log/syslog')
-CMDs.append('sudo chown root:root /var/log/syslog')
+CMDs.append('sudo chmod 640 /var/log/syslog')
+CMDs.append('sudo chown root:adm /var/log/syslog')
 CMDs.append('sudo tail -n 1000 /var/log/daemon.log > /tmp/temp')
 CMDs.append('sudo mv /tmp/temp /var/log/daemon.log')
-CMDs.append('sudo chmod 600 /var/log/daemon.log')
-CMDs.append('sudo chown root:root /var/log/daemon.log')
+CMDs.append('sudo chmod 640 /var/log/daemon.log')
+CMDs.append('sudo chown root:adm /var/log/daemon.log')
 
 
 for cmd in CMDs:
